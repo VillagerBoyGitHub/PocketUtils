ca2430ab062d3b7b4f89ccb0af271e659456dd49
Villager Boy GitHub <103837860+VillagerBoyGitHub@users.noreply.github.com>
2025-03-25 16:17:59 +0100

    Added project to GitHub.

---
+++ src/rob/projects/SecureOP/GenerateOPPassword.php
@@ -0,0 +1,17 @@
+<?php
+namespace rob\projects\SecureOP;
+class GenerateOPPassword {
+  function generateOPPassword () {
+    $alphabet = ["a","s","d","f","g","h","j","k","l","q","w","e","r","t","y","u","i","o","p","z","x","c","v","b","n","m","1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    $password = ""; // Store generated password
+
+    for ($x = 0; $x < 24; $x++) {
+        $random = rand(0, count($alphabet) - 1); // Ensure index is valid
+        $password .= $alphabet[$random]; // Append character to password
+    }
+
+    return $password;
+
+}
+
+}
